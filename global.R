c6b850179eec36726146744f35cfe86fb98a0a85
Jadey Ryan <jryan@agr.wa.gov>
2025-12-11 16:15:24 -0800

    Remove tidyverse

--- global.R
+++ global.R
@@ -1,31 +1,40 @@
 # Load packages
-library(shiny)
-library(shinyWidgets)
 library(bslib)
+library(downloadthis)
+library(dplyr)
+library(flextable)
+library(ggrepel)
+library(glue)
+library(gt)
+library(knitr)
+library(maptiles)
+library(purrr)
+library(quarto)
+library(readxl)
+library(rlang)
+library(rmarkdown)
+library(sever)
+library(shiny)
 library(shinyAce)
-library(shinyjs)
 library(shinyalert)
 library(shinybusy)
-library(sever)
+library(shinyjs)
+library(shinyWidgets)
 library(soils)
+library(stringr)
 library(systemfonts)
-library(readxl)
-library(zip)
-library(tidyverse)
-library(maptiles)
+library(tidyr)
 library(tidyterra)
-library(ggrepel)
-library(gt)
-library(downloadthis)
-library(rmarkdown)
-library(glue)
 library(writexl)
+library(zip)
 
 #for shinyapps.io deployment, make sure soils package is included (not on CRAN)
-options(repos = c(
-  wa = "https://wa-department-of-agriculture.r-universe.dev",
-  CRAN = "https://cloud.r-project.org"
-))
+options(
+  repos = c(
+    wa = "https://wa-department-of-agriculture.r-universe.dev",
+    CRAN = "https://cloud.r-project.org"
+  )
+)
 
 # if (!requireNamespace("soils", quietly = TRUE)) {
 #   remotes::install_github("WA-Department-of-Agriculture/soils")
